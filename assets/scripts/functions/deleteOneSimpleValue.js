e3253bf01290fc80668896662e66edfe0be253da
Loïc Calcagno <calcagnoloic93@gmail.com>
2023-11-30 15:13:20 +0100

    refactoring de main script

--- assets/scripts/functions/deleteOneSimpleValue.js
+++ assets/scripts/functions/deleteOneSimpleValue.js
@@ -1,26 +1,6 @@
-import { displayOnCalculatorScreenForCurrentData } from "./displayOnCalculatorScreen.js";
-
-/**
- * This function retrieves the calculator's 'delete' button from the DOM.
- * An event is then added to the click when calling the function "deleteSimpleValue" to perform
- * the following action: delete a number
- */
-export const handleDelete = () => {
-    const btn_del = document.getElementById("btn-del");
-
-    btn_del.addEventListener("click", () => {
-        deleteSimpleValue();
-    });
-};
-
 /**
  * The purpose of this function is to delete the last element added to the number displayed
  */
-const deleteSimpleValue = () => {
-    const currentData = document.getElementById("currentData");
-    let currentDataValue = currentData.innerText;
-
-    currentDataValue = currentDataValue.slice(0, -1);
-
-    displayOnCalculatorScreenForCurrentData(currentData, currentDataValue);
+export const deleteOneSimpleValue = currentData => {
+    return currentData.slice(0, -1);
 };
--- assets/scripts/functions/displayCalculation.js
+++ assets/scripts/functions/displayCalculation.js
@@ -1,29 +1,7 @@
-import { displayOnCalculatorScreenForCurrentData } from "./displayOnCalculatorScreen.js";
-
-/**
- * This function retrieves the calculator's numeric buttons from the DOM.
- * An event is then added to the click when calling the function "displayCalculation" to perform
- * the following action: concatenate a number
- */
-export const handleOperand = () => {
-    const operands = document.querySelectorAll(".operand");
-
-    operands.forEach(operand => {
-        operand.addEventListener("click", () => {
-            displayCalculation(operand.innerText);
-        });
-    });
-};
-
 /**
  * This function concatenates numbers as the user presses the buttons.
  * @param {string} newValue - Value of added number
  */
-const displayCalculation = newValue => {
-    const currentData = document.getElementById("currentData");
-    let currentDataValue = currentData.innerText;
-
-    currentDataValue = currentDataValue + newValue;
-
-    displayOnCalculatorScreenForCurrentData(currentData, currentDataValue);
+export const displayCalculation = (newValue, currentValue) => {
+    return currentValue + newValue;
 };
